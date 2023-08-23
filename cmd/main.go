f770e7d347bd7656ba21ea80f0bf540083314db1
Gerard Tabone <gerardtabone@yahoo.com>
2023-08-23 11:47:51 +0200

    readme

--- cmd/main.go
+++ cmd/main.go
@@ -1,32 +1,26 @@
 package main
 
-import (
-	"fmt"
-	"phi/parser"
-	"phi/process"
-)
+const program = ` /* SND rule */
+			prc[pid1]: <a, b> <- recv pid2; close self
+			prc[pid2]: send self<pid3, self>
+    `
 
-// const program = ` /* SND rule */
-// 		prc[pid1]: send self<pid3, self>
-// 		prc[pid2]: <a, b> <- recv pid1; close self
-// 		`
-
-const program = ` /* RCV rule */
-		prc[pid1]: send pid2<pid3, self>
-		prc[pid2]: <a, b> <- recv self; close self
-	`
+// const program = ` /* RCV rule */
+// 		prc[pid1]: send pid2<pid3, self>
+// 		prc[pid2]: <a, b> <- recv self; close self
+// `
 
 func main() {
 	// Execute from file
 	// processes, err := parser.ParseFile("cmd/examples/ex1.txt")
 
-	// Execute directly from string
-	processes, err := parser.ParseString(program)
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
-	process.InitializeProcesses(processes, nil)
+	// // Execute directly from string
+	// processes, err := parser.ParseString(program)
+	// if err != nil {
+	// 	fmt.Println(err.Error())
+	// 	return
+	// }
+	// process.InitializeProcesses(processes, nil)
 
 	// Run via API
 	setupAPI()

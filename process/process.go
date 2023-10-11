cc3482b49c8e620f37bbcaf29b39796bb682baea
Gerard Tabone <gerardtabone@yahoo.com>
2023-10-11 08:23:30 +0200

    re-added previously deleted version (which contains parallel priority channels)

--- process/process.go
+++ process/process.go
@@ -1,201 +1,201 @@
 package process
 
-import (
-	"bytes"
-	"strconv"
-)
+// import (
+// 	"bytes"
+// 	"strconv"
+// )
 
-// Stores the states of each running process.
-// May also reference a controller & a monitor to observe the running state.
-type ProcessConfiguration struct {
-	Processes []Process
-	// ref to controller/monitor
-}
+// // Stores the states of each running process.
+// // May also reference a controller & a monitor to observe the running state.
+// type ProcessConfiguration struct {
+// 	Processes []Process
+// 	// ref to controller/monitor
+// }
 
-type Shape int
+// type Shape int
 
-const (
-	LINEAR Shape = iota
-	SHARED
-)
+// const (
+// 	LINEAR Shape = iota
+// 	SHARED
+// )
 
-var shapeMap = map[Shape]string{
-	LINEAR: "prc",
-	SHARED: "sprc",
-}
+// var shapeMap = map[Shape]string{
+// 	LINEAR: "prc",
+// 	SHARED: "sprc",
+// }
 
-// A 'Process' contains the body of the process and the channel it is providing on.
-type Process struct {
-	Body                Form
-	Providers           []Name
-	Shape               Shape
-	FunctionDefinitions *[]FunctionDefinition
-}
+// // A 'Process' contains the body of the process and the channel it is providing on.
+// type Process struct {
+// 	Body                Form
+// 	Providers           []Name
+// 	Shape               Shape
+// 	FunctionDefinitions *[]FunctionDefinition
+// }
 
-func NewProcess(body Form, providers []Name, shape Shape, functionDefinitions *[]FunctionDefinition) *Process {
-	return &Process{
-		Body:                body,
-		Providers:           providers,
-		Shape:               shape,
-		FunctionDefinitions: functionDefinitions,
-	}
-}
-
-func (p *Process) InsertFunctionDefinitions(all *[]FunctionDefinition) {
-	p.FunctionDefinitions = all
-}
-
-func (p *Process) OutlineString() string {
-	var buf bytes.Buffer
-	buf.WriteString(shapeMap[p.Shape])
-	buf.WriteString("[")
-	buf.WriteString(NamesToString(p.Providers))
-	buf.WriteString("]")
-	return buf.String()
-
-}
-
-func (p *Process) String() string {
-	var buf bytes.Buffer
-	buf.WriteString(p.OutlineString())
-	buf.WriteString(": ")
-	buf.WriteString(p.Body.String())
-	return buf.String()
-}
-
-// Name is channel or value.
-type Name struct {
-	// Ident refers to the original name of the channel (used for pretty printing)
-	Ident string
-	// If IsSelf, then this channel should reference the the channel from the provider
-	IsSelf bool
-	// One a channel is initialized (i.e. Channel != nil), the Channel becomes more important than Ident
-	Channel chan Message
-	// // Used for priority commands (e.g. fwd, split, ...)
-	// PriorityChannel chan PriorityMessage
-	// Channel ID is a unique id for each channel
-	// Used only for debugging, since setting the ChannelID is a slow (& synchronous) operation
-	ChannelID uint64
-}
-
-func (n *Name) Initialized() bool {
-	return n.Channel != nil
-}
-
-func (n *Name) String() string {
-	m := ""
-
-	if n.IsSelf {
-		m = "self"
-	} else {
-		if n.Ident == "" {
-			// Set anonymous process names to *
-			m = "*"
-		} else {
-			m = n.Ident
-		}
-	}
-
-	if n.Initialized() {
-		return m + "[" + strconv.FormatUint(n.ChannelID, 10) + "]"
-	} else {
-		return m
-	}
-}
-
-func NamesToString(names []Name) string {
-	var buf bytes.Buffer
-
-	for i, n := range names {
-		buf.WriteString(n.String())
-
-		if i < len(names)-1 {
-			buf.WriteString(", ")
-		}
-	}
-
-	return buf.String()
-}
-
-func (name1 *Name) Equal(name2 Name) bool {
-	if name1.Initialized() && name2.Initialized() {
-		// If the channel is initialized, then only compare the actual channel reference
-		return name1.Channel == name2.Channel
-	}
-	return name1.String() == name2.String() && name1.Initialized() == name2.Initialized()
-}
-
-func (n *Name) Substitute(old, new Name) {
-	if n.Initialized() && n.Channel == old.Channel {
-		// If a channel is initialized, then compare using the channel value
-		n.Channel = new.Channel
-		n.IsSelf = new.IsSelf
-		// n.PriorityChannel = new.PriorityChannel
-		if new.Ident != "" {
-			// not sure if this works
-			n.Ident = new.Ident
-			n.ChannelID = new.ChannelID
-		}
-	} else {
-		if n.Ident == old.Ident {
-			// fmt.Print("\t")
-			// fmt.Print(n.String())
-			// fmt.Println("match")
-			n.Ident = new.Ident
-			n.Channel = new.Channel
-			n.ChannelID = new.ChannelID
-			n.IsSelf = new.IsSelf
-			// n.PriorityChannel = new.PriorityChannel
-		}
-	}
-}
-
-// // Returns channel directly or the provider channel in case of channel called self
-// func (n *Name) GetChannel(p *Process) chan Message {
-// 	if n.IsSelf {
-// 		return p.Provider.Channel
-// 	} else {
-// 		return n.Channel
+// func NewProcess(body Form, providers []Name, shape Shape, functionDefinitions *[]FunctionDefinition) *Process {
+// 	return &Process{
+// 		Body:                body,
+// 		Providers:           providers,
+// 		Shape:               shape,
+// 		FunctionDefinitions: functionDefinitions,
 // 	}
 // }
 
-// func (n *Name) GetPriorityChannel(p *Process) chan PriorityMessage {
+// func (p *Process) InsertFunctionDefinitions(all *[]FunctionDefinition) {
+// 	p.FunctionDefinitions = all
+// }
+
+// func (p *Process) OutlineString() string {
+// 	var buf bytes.Buffer
+// 	buf.WriteString(shapeMap[p.Shape])
+// 	buf.WriteString("[")
+// 	buf.WriteString(NamesToString(p.Providers))
+// 	buf.WriteString("]")
+// 	return buf.String()
+
+// }
+
+// func (p *Process) String() string {
+// 	var buf bytes.Buffer
+// 	buf.WriteString(p.OutlineString())
+// 	buf.WriteString(": ")
+// 	buf.WriteString(p.Body.String())
+// 	return buf.String()
+// }
+
+// // Name is channel or value.
+// type Name struct {
+// 	// Ident refers to the original name of the channel (used for pretty printing)
+// 	Ident string
+// 	// If IsSelf, then this channel should reference the the channel from the provider
+// 	IsSelf bool
+// 	// One a channel is initialized (i.e. Channel != nil), the Channel becomes more important than Ident
+// 	Channel chan Message
+// 	// // Used for priority commands (e.g. fwd, split, ...)
+// 	// PriorityChannel chan PriorityMessage
+// 	// Channel ID is a unique id for each channel
+// 	// Used only for debugging, since setting the ChannelID is a slow (& synchronous) operation
+// 	ChannelID uint64
+// }
+
+// func (n *Name) Initialized() bool {
+// 	return n.Channel != nil
+// }
+
+// func (n *Name) String() string {
+// 	m := ""
+
 // 	if n.IsSelf {
-// 		return p.Provider.PriorityChannel
+// 		m = "self"
 // 	} else {
-// 		return n.PriorityChannel
+// 		if n.Ident == "" {
+// 			// Set anonymous process names to *
+// 			m = "*"
+// 		} else {
+// 			m = n.Ident
+// 		}
+// 	}
+
+// 	if n.Initialized() {
+// 		return m + "[" + strconv.FormatUint(n.ChannelID, 10) + "]"
+// 	} else {
+// 		return m
 // 	}
 // }
 
-// Name is channel or value.
-type Label struct {
-	L string
-}
+// func NamesToString(names []Name) string {
+// 	var buf bytes.Buffer
 
-func (p *Label) String() string {
-	return p.L
-}
+// 	for i, n := range names {
+// 		buf.WriteString(n.String())
 
-func (label1 *Label) Equal(label2 Label) bool {
-	return label1.String() == label2.String()
-}
+// 		if i < len(names)-1 {
+// 			buf.WriteString(", ")
+// 		}
+// 	}
 
-type FunctionDefinition struct {
-	Body         Form
-	FunctionName string
-	Parameters   []Name
-}
+// 	return buf.String()
+// }
 
-func (function *FunctionDefinition) Arity() int {
-	return len(function.Parameters)
-}
+// func (name1 *Name) Equal(name2 Name) bool {
+// 	if name1.Initialized() && name2.Initialized() {
+// 		// If the channel is initialized, then only compare the actual channel reference
+// 		return name1.Channel == name2.Channel
+// 	}
+// 	return name1.String() == name2.String() && name1.Initialized() == name2.Initialized()
+// }
 
-func GetFunctionByNameArity(functions []FunctionDefinition, name string, arity int) *FunctionDefinition {
-	for _, f := range functions {
-		if f.FunctionName == name && f.Arity() == arity {
-			return &f
-		}
-	}
+// func (n *Name) Substitute(old, new Name) {
+// 	if n.Initialized() && n.Channel == old.Channel {
+// 		// If a channel is initialized, then compare using the channel value
+// 		n.Channel = new.Channel
+// 		n.IsSelf = new.IsSelf
+// 		// n.PriorityChannel = new.PriorityChannel
+// 		if new.Ident != "" {
+// 			// not sure if this works
+// 			n.Ident = new.Ident
+// 			n.ChannelID = new.ChannelID
+// 		}
+// 	} else {
+// 		if n.Ident == old.Ident {
+// 			// fmt.Print("\t")
+// 			// fmt.Print(n.String())
+// 			// fmt.Println("match")
+// 			n.Ident = new.Ident
+// 			n.Channel = new.Channel
+// 			n.ChannelID = new.ChannelID
+// 			n.IsSelf = new.IsSelf
+// 			// n.PriorityChannel = new.PriorityChannel
+// 		}
+// 	}
+// }
 
-	return nil
-}
+// // // Returns channel directly or the provider channel in case of channel called self
+// // func (n *Name) GetChannel(p *Process) chan Message {
+// // 	if n.IsSelf {
+// // 		return p.Provider.Channel
+// // 	} else {
+// // 		return n.Channel
+// // 	}
+// // }
+
+// // func (n *Name) GetPriorityChannel(p *Process) chan PriorityMessage {
+// // 	if n.IsSelf {
+// // 		return p.Provider.PriorityChannel
+// // 	} else {
+// // 		return n.PriorityChannel
+// // 	}
+// // }
+
+// // Name is channel or value.
+// type Label struct {
+// 	L string
+// }
+
+// func (p *Label) String() string {
+// 	return p.L
+// }
+
+// func (label1 *Label) Equal(label2 Label) bool {
+// 	return label1.String() == label2.String()
+// }
+
+// type FunctionDefinition struct {
+// 	Body         Form
+// 	FunctionName string
+// 	Parameters   []Name
+// }
+
+// func (function *FunctionDefinition) Arity() int {
+// 	return len(function.Parameters)
+// }
+
+// func GetFunctionByNameArity(functions []FunctionDefinition, name string, arity int) *FunctionDefinition {
+// 	for _, f := range functions {
+// 		if f.FunctionName == name && f.Arity() == arity {
+// 			return &f
+// 		}
+// 	}
+
+// 	return nil
+// }
